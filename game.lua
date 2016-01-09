abcb10fbd7d92245829e9ad35a9df5aa5c0ce6c9
LexLoki <pietroribeiropepe@hotmail.com>
2016-01-09 01:52:08 -0200

    Implementing pause

--- game.lua
+++ game.lua
@@ -5,21 +5,23 @@ require "bullet"
 require "animations"
 
 game = {}
+game.isPaused = false
 
 function game.load()
+  game.pauseImg = love.graphics.newImage("/Assets/Tela_Intro/Pause.png")
   stage.load()
   player.load()
   enemies.load()
   bullet.load()
-  --dialog.load()
-  --dText = dialog.newDialogText("Hey man, come right here\nim gonna punch you in the face",0,0)
   game.startTime = 2
   game.startAnim = animComp.newAnim(12,player.sprites[run].time)
   animations.load()
 end
 
 function game.keypressed(key)
-  if not(game.startTime>0) then
+  if key=="escape" or key=="return" then
+    game.isPaused = not game.isPaused
+  elseif not game.isPaused and not(game.startTime>0) then
     player.keypressed(key)
   end
 end
@@ -28,12 +30,13 @@ function game.mousepressed(x,y,button)
 end
 
 function game.update(dt)
-  stage.update(dt)
-  if game.startTime>0 then game.startUpdate(dt) else player.update(dt) end
-  enemies.update(dt)
-  bullet.update(dt)
-  --dialog.updateText(dt,dText)
-  animations.update(dt)
+  if not game.isPaused then
+    stage.update(dt)
+    if game.startTime>0 then game.startUpdate(dt) else       player.update(dt) end
+    enemies.update(dt)
+    bullet.update(dt)
+    animations.update(dt)
+  end
 end
 
 function game.draw()
@@ -41,8 +44,8 @@ function game.draw()
   if game.startTime>0 then game.startDraw() else player.draw() end
   enemies.draw()
   bullet.draw()
-  --dialog.drawText(dText)
   animations.draw()
+  game.pauseDraw()
 end
 
 function game.startUpdate(dt)
@@ -59,6 +62,7 @@ function game.startDraw()
   local sprite = player.sprites[run]
   love.graphics.draw(sprite.sheet, sprite.quads[game.startAnim.curr_frame],player.x,player.y,0,player.scale,player.scale,player.offset.x,player.offset.y)
   shield.draw()
+  game.pauseDraw()
 end
 
 function game.prepareBackgrounds(data)
@@ -71,4 +75,15 @@ end
 
 function game.start()
   stage.start()
+end
+
+function game.pauseDraw()
+  if game.isPaused then
+    local w = love.graphics.getWidth()
+    local h = love.graphics.getHeight()
+    love.graphics.setColor(0,0,0,100)
+    love.graphics.rectangle("fill",0,0,w,h)
+    love.graphics.setColor(255,255,255)
+    love.graphics.draw(game.pauseImg,0,0,0,w/game.pauseImg:getWidth(),h/game.pauseImg:getHeight())
+  end
 end
